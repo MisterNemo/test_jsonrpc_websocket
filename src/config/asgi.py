5ee9a94bb2507dd69829c83ad02a5f108b59469f
Nurbek <nmoldagaliyev@gmail.com>
2021-09-15 18:54:13 +0600

    add postgresql container, add djoser in settings

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,11 +1,13 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chats.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chats.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
